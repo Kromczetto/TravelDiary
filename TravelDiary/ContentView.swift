3a3c2b27767b1b1774eecd773985f2dacfb10460
Kromczetto <jakubkromolowski@gmail.com>
2025-02-16 19:33:27 +0100

    Initial Commit

---
+++ TravelDiary/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TravelDiary
+//
+//  Created by Kuba Kromomołowski on 16/02/2025.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelDiary/TravelDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiaryApp.swift
+//  TravelDiary
+//
+//  Created by Kuba Kromomołowski on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
