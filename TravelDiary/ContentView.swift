113a7ff2828077417813cee79fb19a35e599ebef
Kromczetto <jakubkromolowski@gmail.com>
2025-02-16 22:13:43 +0100

    Basic structure

--- TravelDiary/ContentView.swift
+++ TravelDiary/ContentView.swift
@@ -2,24 +2,63 @@
 //  ContentView.swift
 //  TravelDiary
 //
-//  Created by Kuba Kromomołowski on 16/02/2025.
+//  Created by Kuba Kromołowski on 16/02/2025.
 //
 
 import SwiftUI
 import ComposableArchitecture
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+@Reducer
+struct PlaceFeature {
+    @ObservableState
+    struct State {
+        var placeName: String
+        var photo: String
+        var rating: Double
+        var feelings: String
+        init(placeName: String = "", photo: String = "",
+             rating: Double = 0.0, feelings: String = "") {
+            self.placeName = placeName
+            self.photo = photo
+            self.rating = rating
+            self.feelings = feelings
         }
-        .padding()
+    }
+    
+    enum Action: Equatable, BindableAction {
+        case binding(BindingAction<State>)
+    }
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .binding(\.placeName):
+                print("PlaceName: \(state.placeName)")
+                return .none
+            case .binding(\.photo):
+                print("Photo: \(state.photo)")
+                return .none
+            case .binding(_):
+                return .none
+            }
+        }
     }
 }
-
+struct ContentView: View {
+    @State var store: StoreOf<PlaceFeature>
+    
+    var body: some View {
+        VStack {
+            TextField("Place name", text: $store.placeName)
+            TextField("Photo", text: $store.photo)
+        }
+    }
+}
 #Preview {
-    ContentView()
+    ContentView(
+    store: Store(initialState: PlaceFeature.State()) {
+        PlaceFeature()
+    }
+  )
 }
---
+++ TravelDiary/Memories.swift
@@ -0,0 +1,10 @@
+//
+//  Memories.swift
+//  TravelDiary
+//
+//  Created by Kuba Kromołowski on 16/02/2025.
+//
+struct Memories: Codable {
+    var name: String
+    
+}
